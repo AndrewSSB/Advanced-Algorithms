f496cdd8618e7a651ae02a578322a2c1cacb1363
Andrei Ene <95134095+AndrewSSB@users.noreply.github.com>
2022-04-29 12:54:13 +0300

    Update Test_Orientare_first_aproach.cpp

--- Lab5/Test_Orientare_first_aproach.cpp
+++ Lab5/Test_Orientare_first_aproach.cpp
@@ -16,27 +16,24 @@ struct coords{
     }
 };
 
-string direction(coords A, coords B, coords C){
+void direction(coords A, coords B, coords C){
     int val = (A.x - B.x)*(B.y - C.y) - (A.y - B.y)*(B.x - C.x);
-    if (val == 0) return "TOUCH";
-    return (val < 0) ? "RIGHT" : "LEFT";
+    if (val == 0) cout << "TOUCH" << endl;
+    if (val < 0) cout << "RIGHT" << endl; 
+    else cout << "LEFT" << endl;
 }
 
 int main()
 {
     int t;
-    cout << "t: ";
     cin >> t;
 
-    coords _coords[3];
-    vector<string> aux;
+    coords P, Q, R;
 
     for (int i = 0; i < t; i++){
-        cin >> _coords[0] >> _coords[1] >> _coords[2];
-        aux.push_back(direction(_coords[0], _coords[1], _coords[2]));
+        cin >> P >> Q >> R;
+        direction(P, Q, R);
     }
-
-    for (auto x : aux)
-        cout << x << "\n";
+    
     return 0;
 }
